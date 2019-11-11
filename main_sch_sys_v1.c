983140d07044076ce6ea834291d9ca33d1065c1e
Helmut Ngawa <ngawahelmut@yahoo.de>
2019-11-11 10:38:06 +0100

    Add basis file structur for school billing system. No implementation yet!

---
+++ main_sch_sys_v1.c
@@ -0,0 +1,25 @@
+	/** $Revision: 1.1 $
+	* @date: 11/11/19
+	* @file main_sch_sys_v1.c
+	*
+	* @brief Main function to execute the functions of the mini project 
+	* <a>School Billing System</a>.
+	*
+	* @note
+	* - 
+	* - 
+	*
+********************************************************************************/
+
+#include <stdio.h> /* Standard input/output definitions */
+#include <string.h>  /* String function definitions */
+#include <stdlib.h>  /* Standard library, memory allocation */
+
+#include "school_system_v1.h" /* Function and constance definitions */
+
+int main(int argc, char* argv[])
+{
+	printf("Hello World!\n");
+
+	return 0;
+}
