26362653e61280e63e22537e5cdc1b90bde14a50
Landro82 <44756805+Landro82@users.noreply.github.com>
2019-11-13 14:54:46 +0100

    Create Menu.c

---
+++ Menu.c
@@ -0,0 +1,41 @@
+void menu(){
+    //please enter the choice
+    printf("1:: Add record\n");
+    printf("2:: Search record\n");
+    printf("3:: Modifier record\n");
+    printf("4:: Delete record\n");
+    printf("5:: Calculed fees\n");
+    printf("6:: Exit\n");
+    // Enter  choice
+    int choice;
+    printf("Enter you choice \n");
+    scanf("%d", &choice);
+    switch(choice){
+    case 1:
+        printf("1:: Add record\n");
+        addDat();
+        break;
+    case 2:
+        printf("2:: Search record\n");
+        searchDat();
+        break;
+    case 3:
+        printf("3:: Modifier record\n");
+        modifyDat();
+        break;
+    case 4:
+        printf("4:: Delete record\n");
+        deleteDat();
+        break;
+    case 5:
+        printf("5:: Calculed fees\n");
+        calculFees();
+        break;
+    case 6:
+        printf("6:: Exit\n");
+        Exit();
+     default:
+            printf(" Invalid choice\n");
+            break;
+    }
+}
