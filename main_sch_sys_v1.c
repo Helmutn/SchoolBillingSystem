3d21c4d6ef6a59e855e7fe14f2ca255823756d27
Landro82 <44756805+Landro82@users.noreply.github.com>
2019-11-13 13:54:50 +0100

    Create main_sch_sys_v1.c

---
+++ main_sch_sys_v1.c
@@ -0,0 +1,145 @@
+#include <stdio.h> /* Standard input/output definitions */
+#include <string.h>  /* String function definitions */
+#include <stdlib.h>  /* Standard library, memory allocation */
+
+#include "school_system_v1.h" /* Function and constance definitions */
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/** 
+ * Function call - 'menu()'
+ *
+ * @brief choose which function to execute
+ *
+ * @param None
+ *
+ * @note 
+ *
+ * @author 
+ *
+ * @return None
+ */
+void menu()
+{
+
+}
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/** 
+ * Function call - 'recordDat()'
+ *
+ * @brief store the data type of student and teacher
+ *
+ * @param 
+ *
+ * @note None
+ *
+ * @author 
+ *
+ * @return 
+ */
+void recordDat();
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/** 
+ * Function call - 'checkDat()'
+ *
+ * @brief 
+ *
+ * @param 
+ *
+ * @note 
+ *
+ * @author 
+ *
+ * @return 
+ */
+void checkDat();
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/** 
+ * Function call - 'addDat(ID,name)'
+ *
+ * @brief add information about student or teacher
+ *
+ * @param ID descript the student or the teacher
+ *
+ * @note None
+ *
+ * @author 
+ *
+ * @return 
+        print all data about the student or teacher 
+        otherwise add a new student or teacher 
+ */
+void addDat();
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/** 
+ * Function call - 'searchDat(ID)'
+ *
+ * @brief search information about student or teacher
+ *
+ * @param ID give description about student or the teacher
+ *
+ * @note 
+ *
+ * @author 
+ *
+ * @return 
+      print all data about the student or teacher 
+       otherwise print error 
+
+ */
+void searchDat();
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/** 
+ * Function call - 'modifyDat(ID)'
+ *
+ * @brief modifier an existing data
+ *
+ * @param ID give description about student or the teacher
+ *
+ * @note 
+ *
+ * @author 
+ *
+ * @return 
+      print all data about the student or teacher 
+       otherwise print error
+ */
+void modifyDat();
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/** 
+ * Function call - 'deleteDat(ID)'
+ *
+ * @brief delete data an existing data
+ *
+ * @param ID give description about student or the teacher
+ *
+ * @note 
+ *
+ * @author 
+ *
+ * @return 
+       print data about the student or teacher 
+       otherwise print error data not found 
+ */
+void deleteDat();
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/** 
+ * Function call - 'exit()'
+ *
+ * @brief 
+ *
+ * @param 
+ *
+ * @note 
+ *
+ * @author 
+ *
+ * @return 
+ */
+void exit();
